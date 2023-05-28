94528d0696d58f1b05fbf63c85565be4b4a47b6f
k02s <khsaleh022@gmail.com>
2023-05-28 17:57:54 +0000

    solve lab 10

--- js/app.js
+++ js/app.js
@@ -7,55 +7,55 @@ const allEmployees = [];
 // method to calaulate salary and netSalary
 function calculateSalary(employeeLevel) {
 
-    let salary = 0;
-    let netSalary = 0;
-  
-    switch (employeeLevel) {
-      case "Senior":
-        salary = Math.ceil(1500 + Math.random() * (2000 - 1500 + 1));
-        break;
-      case "Mid-Senior":
-        salary = Math.ceil(1000 + Math.random() * (1500 - 1000 + 1));
-        break;
-      case "Junior":
-        salary = Math.ceil(500 + Math.random() * (1000 - 500 + 1));
-        break;
-    }
-    netSalary = salary - (salary * 0.075);
-    return netSalary;
+  let salary = 0;
+  let netSalary = 0;
+
+  switch (employeeLevel) {
+    case "Senior":
+      salary = Math.ceil(1500 + Math.random() * (2000 - 1500 + 1));
+      break;
+    case "Mid-Senior":
+      salary = Math.ceil(1000 + Math.random() * (1500 - 1000 + 1));
+      break;
+    case "Junior":
+      salary = Math.ceil(500 + Math.random() * (1000 - 500 + 1));
+      break;
   }
-  
+  netSalary = salary - (salary * 0.075);
+  return netSalary;
+}
+
 
 // method to genarate ID
 function generateEmployeeID() {
 
-    employeeID++;
-    return employeeID;
+  employeeID++;
+  return employeeID;
 
 }
 
 // create objects by constructor
 function Employee(ID, name, department, level, imageURL, salary) {
 
-    this.employeeID = generateEmployeeID();
-    this.fullName = name;
-    this.department = department;
-    this.level = level;
-    this.imageURL = imageURL;
-    this.salary = calculateSalary(level);
+  this.employeeID = generateEmployeeID();
+  this.fullName = name;
+  this.department = department;
+  this.level = level;
+  this.imageURL = imageURL;
+  this.salary = calculateSalary(level);
 
-    allEmployees.push(this);
+  allEmployees.push(this);
 
 }
 
 
-let emp1 = new Employee(generateEmployeeID(), 'Ghazi Samer', 'Administration', 'Senior', 'https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', calculateSalary('Senior'))
-let emp2 = new Employee(generateEmployeeID(), 'Lana Ali', 'Finance', 'Senior', 'https://images.pexels.com/photos/1586973/pexels-photo-1586973.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Senior'))
-let emp3 = new Employee(generateEmployeeID(), 'Tamara Ayoub', 'Marketing', 'Senior', 'https://images.pexels.com/photos/3747446/pexels-photo-3747446.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Senior'))
-let emp4 = new Employee(generateEmployeeID(), 'Safi Walid', 'Administration', 'Mid-Senior', 'https://images.pexels.com/photos/927451/pexels-photo-927451.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Mid-Senior'))
-let emp5 = new Employee(generateEmployeeID(), 'Omar Zaid', 'Development', 'Senior', 'https://images.pexels.com/photos/927451/pexels-photo-927451.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Senior'))
-let emp6 = new Employee(generateEmployeeID(), 'Rana Saleh', 'Development', 'Junior', 'https://images.pexels.com/photos/1181414/pexels-photo-1181414.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Junior'))
-let emp7 = new Employee(generateEmployeeID(), 'Hadi Ahmad', 'Finance', 'Mid-Senior', 'https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg?auto=compress&cs=tinysrgb&w=600', calculateSalary('Mid-Senior'))
+let emp1 = new Employee(generateEmployeeID(), 'Ghazi Samer', 'Administration', 'Senior', 'assets/Ghazi.jpg', calculateSalary('Senior'))
+let emp2 = new Employee(generateEmployeeID(), 'Lana Ali', 'Finance', 'Senior', 'assets/Lana.jpg', calculateSalary('Senior'))
+let emp5 = new Employee(generateEmployeeID(), 'Omar Zaid', 'Development', 'Senior', 'assets/Omar.jpg', calculateSalary('Senior'))
+let emp3 = new Employee(generateEmployeeID(), 'Tamara Ayoub', 'Marketing', 'Senior', 'assets/Tamara.jpg', calculateSalary('Senior'))
+let emp4 = new Employee(generateEmployeeID(), 'Safi Walid', 'Administration', 'Mid-Senior', 'assets/Safi.jpg', calculateSalary('Mid-Senior'))
+let emp7 = new Employee(generateEmployeeID(), 'Hadi Ahmad', 'Finance', 'Mid-Senior', 'assets/Hadi.jpg', calculateSalary('Mid-Senior'))
+let emp6 = new Employee(generateEmployeeID(), 'Rana Saleh', 'Development', 'Junior', 'assets/Rana.jpg', calculateSalary('Junior'))
 
 
 
@@ -73,11 +73,62 @@ console.log(emp7);
 // render method 
 Employee.prototype.render = function () {
 
-    document.write(`<h4>Employee name:   ${this.fullName} <br> Department:   ${this.department} <br> Employee Salary:  ${this.salary} <br><br></h4>`)
+  // document.write(`<h4>Employee name: ${this.fullName} <br> Department: ${this.department} <br> Employee Salary: ${this.salary} <br><br></h4>`) 
+
+  // create div (it will be a container for card) element 
+  let card = document.createElement("div");
+  // append element to body 
+  document.body.appendChild(card);
+  // create and append image
+
+  let image = document.createElement("img");
+  image.src = this.imageURL;
+  image.style.width = '100%';
+  image.style.height = 'auto';
+  card.appendChild(image);
+
+  // create and append name 
+  let name = document.createElement("p");
+  name.textContent = `Name: ${this.fullName}`;
+  card.appendChild(name);
+
+  // create and append ID 
+  let ID = document.createElement("p");
+  ID.textContent = `ID: ${this.employeeID}`;
+  card.appendChild(ID);
+
+  // create and append department 
+  let department = document.createElement("p");
+  department.textContent = `Department: ${this.department}`;
+  card.appendChild(department);
+
+  // create and append level
+  let level = document.createElement("p");
+  level.textContent = `Level: ${this.level}`;
+  card.appendChild(level);
+
+  // create and append salary
+  let salary = document.createElement("p");
+  salary.textContent = `${this.salary}`;
+  card.appendChild(salary);
+
+  // style card
+  card.style.border = "1px solid black";
+  card.style.borderRadius = '8px';
+  card.style.padding = '16px';
+  card.style.margin = '8px';
+  card.style.marginLeft = '25px';
+  card.style.marginRight = '25px';
+  card.style.width = '230px';
+  card.style.backgroundColor = '#A4D0A4';
+  card.style.display = 'inline-block';
+
 
 }
 
 
 for (let i = 0; i < allEmployees.length; i++) {
-    allEmployees[i].render();
+
+  allEmployees[i].render();
 }
+
